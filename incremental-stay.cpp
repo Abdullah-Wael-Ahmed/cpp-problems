ed9d169a0b3b723f2367a09963278a6b1e6bd597
Obaida <91075544+Abdullah-Wael-Ahmed@users.noreply.github.com>
2025-10-05 13:16:13 +0300

    Update incremental-stay.cpp

--- incremental-stay.cpp
+++ incremental-stay.cpp
@@ -3,21 +3,24 @@
 using namespace std;
 
 // https://codeforces.com/problemset/problem/2151/C
-// still time limit exceeded, have to explore "dynamic programming"
+// time limit exceeded on test 8
 
 int maxStayCalc(int sensorDetects, vector<int> seconds);
 
-int main() {
+int main()
+{
 
     int numberOfTestCases;
 
     cin >> numberOfTestCases;
 
-    for (int i = 0 ; i < numberOfTestCases ; i++){
+    for (int i = 0; i < numberOfTestCases; i++)
+    {
         int sensorDetects;
         cin >> sensorDetects;
         vector<int> seconds(sensorDetects * 2);
-        for (int j = 0 ; j < sensorDetects * 2 ; j++){
+        for (int j = 0; j < sensorDetects * 2; j++)
+        {
             cin >> seconds[j];
         }
         maxStayCalc(sensorDetects, seconds);
@@ -27,22 +30,20 @@ int main() {
     return 0;
 }
 
-int maxStayCalc(int sensorDetects, vector<int> seconds){
-    for (int k = 1 ; k <= sensorDetects ; k++){
-        long long stayingPeopleEntrySum = 0, stayingPeopleLeavingSum = 0, sum = 0;
-        for (int i = 1 ; i <= sensorDetects * 2 ; i++){
-            if (i <= k - 1){
-                stayingPeopleEntrySum += seconds[i - 1];
-            } else if (i > (sensorDetects * 2) - (k - 1)){
-                stayingPeopleLeavingSum += seconds[i - 1];
-            } else {
-                sum += seconds[i] - seconds[i - 1];
-                i++;
-            }
+int maxStayCalc(int sensorDetects, vector<int> seconds)
+{
+    long long stayingPeopleSum = 0;
+    for (int k = 1; k <= sensorDetects; k++)
+    {
+        long long sum = 0;
+        sum += stayingPeopleSum;
+        for (int i = k; i <= (sensorDetects * 2) - (k - 1); i++)
+        {
+            sum += seconds[i] - seconds[i - 1];
+            i++;
         }
-        sum += stayingPeopleLeavingSum - stayingPeopleEntrySum;
+        stayingPeopleSum += seconds[(sensorDetects * 2) - (k)] - seconds[k - 1];
         cout << sum << " ";
     }
     return 0;
-
 }
